db2f4cd2f126daa26b27a968364b478f584099ea
zengn <zengn@xiaopeng.com>
2024-09-12 10:22:06 +0800

    一级路由

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <el-button type="primary">i am button</el-button>
+  <!-- 一级路由出口 -->
+  <RouterView/>
   <!-- <header>
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
