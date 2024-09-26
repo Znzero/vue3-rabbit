c4c23545ba4f9cb1ef8dfcb44f8122be31d9c4af
zengn <zengn@xiaopeng.com>
2024-09-26 16:18:39 +0800

    面板组件封装

--- src/components/Home/index.vue
+++ src/components/Home/index.vue
@@ -4,6 +4,7 @@ import HomeBanner from './components/HomeBanner.vue'
 import HomeNew from './components/HomeNew.vue'
 import HomeHot from './components/HomeHot.vue'
 import homeProduct from './components/HomeProduct.vue'
+import HomePanel from './components/HomePanel.vue'
 </script>
 
 <template>
@@ -14,4 +15,14 @@ import homeProduct from './components/HomeProduct.vue'
   <HomeNew />
   <HomeHot />
   <homeProduct />
+  <HomePanel title="新鲜好物" sub-title="新鲜好物 好多商品">
+    <div>
+      好物推荐
+    </div>
+  </HomePanel>
+  <HomePanel title="人气推荐" sub-title="人气爆款 不容错过">
+    <div>
+      人气好物
+    </div>
+  </HomePanel>
 </template>
