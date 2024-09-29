0eac6d00e218c210c881000003f068778062a842
zengn <zengn@xiaopeng.com>
2024-09-29 18:06:22 +0800

    二级面包屑导航实现

--- src/components/SubCategory/index.vue
+++ src/components/SubCategory/index.vue
@@ -1,31 +1,27 @@
 <script setup>
-
-
+import { getCategoryFilterAPI } from '@/api/category'
+import { ref } from 'vue'
+import { useRoute } from 'vue-router'
+// 获取面包屑导航数据
+const filterData = ref({})
+const route = useRoute()
+const getFilterData = async () => {
+  const res = await getCategoryFilterAPI(route.params.id)
+  filterData.value = res.result
+  console.log(res)
+}
+getFilterData()
 </script>
 
 <template>
-  <div class="container ">
-    <!-- 面包屑 -->
-    <div class="bread-container">
-      <el-breadcrumb separator=">">
-        <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
-        <el-breadcrumb-item :to="{ path: '/' }">居家
-        </el-breadcrumb-item>
-        <el-breadcrumb-item>居家生活用品</el-breadcrumb-item>
-      </el-breadcrumb>
-    </div>
-    <div class="sub-container">
-      <el-tabs>
-        <el-tab-pane label="最新商品" name="publishTime"></el-tab-pane>
-        <el-tab-pane label="最高人气" name="orderNum"></el-tab-pane>
-        <el-tab-pane label="评论最多" name="evaluateNum"></el-tab-pane>
-      </el-tabs>
-      <div class="body">
-         <!-- 商品列表-->
-      </div>
-    </div>
+  <div class="bread-container">
+    <el-breadcrumb separator=">">
+      <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item :to="{ path: `/category/${filterData.parentId}` }">{{ filterData.parentName }}
+      </el-breadcrumb-item>
+      <el-breadcrumb-item>{{ filterData.name }}</el-breadcrumb-item>
+    </el-breadcrumb>
   </div>
-
 </template>
 
 
